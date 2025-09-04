6b4ba633e3c8460b89585de3fb98da47c34e2a1a
Anjan Suman <anjansuman80@gmail.com>
2025-09-05 00:35:09 +0530

    starting fresh.

--- programs/swe-project-1/src/state/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod platform_config;
-pub mod property;
-pub mod booking;
-
-pub use platform_config::{PlatformConfig};
-pub use property::{Property};
-pub use booking::{Booking, BookingStatus};
--- programs/swe-project-1/src/state/platform_config.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-
-#[account]
-pub struct PlatformConfig {
-    pub authority: Pubkey,
-    pub platform_fee: u64,
-    pub bump: u8
-}
